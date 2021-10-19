fab9856807f537ccf30d65363bf2ac53137c6ab6
tiko02220 <tiko02220@gmail.com>
2021-10-19 15:50:18 +0400

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,59 +1,77 @@
 package com.company;
+
+
+class Invoice {
+    private String partNm;//partNm = partNumber
+    private String partDs;//partDs = partDescription
+    private int quantity;
+    private double price;
+
+    Invoice(String partNm, String partDs, int quantity, double price) {
+        this.partDs = partDs;
+        this.partNm = partNm;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void setPartNm(String partNm) {
+        this.partNm = partNm;
+    }
+
+    public String getPartNm() {
+        return partNm;
+    }
+
+    public void setPartDs(String partDs) {
+        this.partDs = partDs;
+    }
+
+    public String getPartDs() {
+        return partDs;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity > 0) {
+            this.quantity = quantity;
+        } else this.quantity = 0;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+        } else this.price = 0.0;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double totalInvoice() {
+        return quantity * price;
+    }
+
+    public void viewInvoice() {
+        System.out.println("Number of part- " + getPartNm());
+        System.out.println("Description- " + getPartDs());
+        System.out.println("Sold items- " + getQuantity());
+        System.out.println("Price of one item- " + getPrice() + "$");
+        System.out.println("Total Invoice- " + totalInvoice() + "$");
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-         Invoice invoice = new Invoice("Default","Default",0,0);
-         invoice.setPartNumber("001A");
-         invoice.setPartDescription("Phone");
-         invoice.setQuantity(10);
-         invoice.setPrice(20.10);
-         invoice.ViewInvoice();
-     }
- }
-class  Invoice{
-     private String  PartNumber;
-     private String  PartDescription;
-     private int Quantity;
-     private double Price;
-     Invoice ( String PartNumber, String PartDescription,int Quantity,double Price){
-        this.PartDescription = PartDescription;
-        this.PartNumber = PartNumber;
-        this.Quantity = Quantity;
-        this.Price = Price;
-    }
-    public void setPartNumber(String partNumber){
-        this.PartNumber = partNumber;
-    }
-    public String getPartNumber(){
-        return  PartNumber;
-    }
-    public void setPartDescription(String PartDescription){
-        this.PartDescription =PartDescription;
-    }
-    public String getPartDescription(){
-        return  PartDescription;
-    }
-    public void setQuantity(int Quantity){
-          if(Quantity>0){
-          this.Quantity = Quantity;
-         }else this.Quantity = 0;
-    }
-    public int getQuantity(){
-         return  Quantity;
-    }
-    public void setPrice(double Price){
-         if(Price>0){
-         this.Price = Price;
-         }else this.Price = 0;
-     }
-    public double getPrice(){
-         return Price;
-    }
-    public double TotalInvoice(){
-         return Quantity * Price;
-    }
-    public void ViewInvoice(){
-      System.out.println("Number of part- "+getPartNumber()+"\n"+"Description- "+getPartDescription()+"\n"
-     +"Sold items- "+getQuantity()+"\n"+"Price of one item- "+getPrice()+"$"+"\n"+"Total Invoice- "+TotalInvoice()+"$");
+        Invoice invoice = new Invoice("Default", "Default", 0, 0.0);
+        invoice.setPartNm("001A");
+        invoice.setPartDs("Phone");
+        invoice.setQuantity(10);
+        invoice.setPrice(20.10);
+        invoice.viewInvoice();
     }
 }
 
